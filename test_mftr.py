11af85e3e205253dc6d8bb9944a3204d615ce4d3
jarbassaidai <jrubenst10@gmail.com>
2017-10-07 14:46:42 -0600

    baby steps into testing

--- test_mftr.py
+++ test_mftr.py
@@ -7,7 +7,7 @@ import argparse
 
 
 class templateA(unittest.TestCase):
-    def __init(self,method):
+    def __init__(self,method):
         self.mftr = None
         self.setup()
 
@@ -30,7 +30,32 @@ class templateA(unittest.TestCase):
         print('teardown\n')
 
 class test_compRegex(templateA):  #unittest.TestCase):
-    def test_notComplete(self):
+    def setup(self):
+        self.parser = argparse.ArgumentParser(description='token replace in multiple files')
+        self.parser.add_argument('--directory', dest='dir',  required=True,  default = '.', help= 'directory to run threw')
+        self.parser.add_argument('--token',  dest='token', required=True, default = '192.168.0.0', help= 'token to look for')
+        self.parser.add_argument('--replace', dest='replace', required=True, default = '192.168.1.1', help='replacement value' )
+        self.parser.add_argument('--rewrite', dest='rewrite', default=True, help='allow different sized search and replace items ')
+        self.parser.add_argument('--backup', dest='backup', default=True,  help='create backup of original file')
+        self.parser.add_argument('--log', dest='log', default = 'mftr_change.log',  help='log file to log all changes')
+        self.parser.add_argument('--skip', dest='skip', nargs='+', default ='png jpg exe jar z gz mftr_bck', help='list of file types that are not searched')
+        self.parser.add_argument('--include',dest='include', nargs='+', default = '*' , help='list of file types to incluce')
+        self.parser.add_argument('--revert',dest='rewritevert', default = False, help='rename all the .mftr_bck files to original')
+        self.args = self.parser.parse_args()
+        self.mftr = None
+        self.mftr = multiFileTokenReplace(self.args)
+
+    def test_token(self):
+        self.args.token = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+        try:
+            self.comRegx(self.args)
+        except :
+            self.assertTrue(False)
+
+    def test_skip(self):
+        self.assertTrue(False)
+
+    def test_include(self):
         self.assertTrue(False)
 
 class test_init(templateA):
